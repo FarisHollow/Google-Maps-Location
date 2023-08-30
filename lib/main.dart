250805eac60514b8e54c20841d2b1ffdd2a21171
FarisHollow <mirfaris79@gmail.com>
2023-08-31 03:25:15 +0600

    Google maps

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomeScreen()
+    );
+  }
+}
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Google Maps"),
+      ),
+      body: const GoogleMap(
+        initialCameraPosition: CameraPosition(target: LatLng(23.85580349118658, 90.40019094936395)),
+      ),
+    );
+  }
+}
+
